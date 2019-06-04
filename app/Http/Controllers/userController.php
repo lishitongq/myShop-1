220ae14fc11c96653311e7e41a05330ea620f0db
mnmnwq <mnmnwq@163.com>
2019-06-04 16:18:20 +0800

    181

--- app/Http/Controllers/userController.php
+++ app/Http/Controllers/userController.php
@@ -7,10 +7,7 @@ use Illuminate\Http\Request;
 class userController extends Controller
 {
     public function index(){
-    	for($i=1;$i<10;$i++){
-    		echo $i;
-    		echo "<br/>";
-    	}
+    	
     	return view('welcome');
     }
 }
