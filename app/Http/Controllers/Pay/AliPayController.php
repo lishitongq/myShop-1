f17d22d140641c0f8c2e6fa900452efb4ac8db4b
mnmnwq <mnmnwq@163.com>
2019-06-25 14:22:08 +0800

    aa

--- app/Http/Controllers/Pay/AliPayController.php
+++ app/Http/Controllers/Pay/AliPayController.php
@@ -298,7 +298,7 @@ class AliPayController extends BasicController
         ($res) or die('支付宝RSA公钥错误。请检查公钥文件格式是否正确');
         //调用openssl内置方法验签，返回bool值
         $result = (bool)openssl_verify($this->getSignContent($params), base64_decode($sign), $res, OPENSSL_ALGO_SHA256);
-
+        \Log::Info($result);
         if(!$this->checkEmpty($this->aliPubKey)){
             openssl_free_key($res);
         }
